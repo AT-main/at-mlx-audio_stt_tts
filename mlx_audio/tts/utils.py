602e3168fb158827123c7769d6f34e7a47a745ad
Amir <amirtajik@gmail.com>
2025-06-11 12:07:57 -0400

    refactor: remove unused save_model import and comment out save_model call in convert function

--- mlx_audio/tts/utils.py
+++ mlx_audio/tts/utils.py
@@ -12,7 +12,7 @@ from huggingface_hub import snapshot_download
 from mlx import nn
 from mlx.utils import tree_flatten
 from mlx_lm.convert import mixed_quant_predicate_builder
-from mlx_lm.utils import dequantize_model, quantize_model, save_config, save_model
+from mlx_lm.utils import dequantize_model, quantize_model, save_config
 from transformers import AutoConfig
 
 MODEL_REMAPPING = {"outetts": "outetts", "spark": "spark", "sam": "sesame"}
@@ -434,7 +434,7 @@ def convert(
             dest_dir.mkdir(parents=True, exist_ok=True)
             shutil.copy(file, dest_dir)
 
-    save_model(mlx_path, model, donate_model=True)
+    # save_model(mlx_path, model, donate_model=True)
 
     save_config(config, config_path=mlx_path / "config.json")
 
